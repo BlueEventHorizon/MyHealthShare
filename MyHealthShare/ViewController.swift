2924ddfc2da530066bd201133433d7c90973b54d
k_terada <k_terada@yumemi.co.jp>
2018-11-30 10:26:56 +0900

    initial commit

---
+++ MyHealthShare/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyHealthShare
+//
+//  Created by 寺田 克彦 on 2018/11/30.
+//  Copyright © 2018 beowulf-tech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
